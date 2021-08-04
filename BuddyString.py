1547c35a55f47f016eee3b748f31825883ff7b2b
coderwenwen <wangwenusa11@gmail.com>
2021-08-04 00:06:05 -0700

    Still has failed test

--- BuddyString.py
+++ BuddyString.py
@@ -13,19 +13,29 @@ class BuddyString(object):
             i = 0
             lstS = list(s)
             lstGoal = list(goal)
+            isSwapped = False
+            hasSameCharacter = False
             while i < len(lstS):
                 if lstS[i] != lstGoal[i]:
+                    isSwapped = True
                     temp = lstS[i]
                     lstS[i] = lstGoal[i]
                     lstGoal[i] = temp
                 i = i + 1
 
-            if ''.join(lstS) == ''.join(goal):
+            if ''.join(lstS) == ''.join(goal) and isSwapped is True:
                 return True
         return False
 
 class BuddyStringTest(unittest.TestCase):
     def test_EasyCase(self):
         self.assertEqual(BuddyString.buddyStrings(self, "abab", "abab"), True)
+
+    def test_EasyCase2(self):
+        self.assertEqual(BuddyString.buddyStrings(self, "ab", "ab"), False)
+
+    def test_EasyCase3(self):
+        self.assertEqual(BuddyString.buddyStrings(self, "aa", "aa"), True)
+
     def test_RepeatedWord(self):
         self.assertEqual(BuddyString.buddyStrings(self, "aaaaaaabc", "aaaaaaacb"), True)
