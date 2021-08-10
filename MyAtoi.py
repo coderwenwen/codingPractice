82d6d89c925fc46bba8d1e8ed807ac63c141f0ad
coderwenwen <wangwenusa11@gmail.com>
2021-08-09 23:10:25 -0700

    Added myatoi

--- MyAtoi.py
+++ MyAtoi.py
@@ -1,7 +1,19 @@
+import unittest
 class MyAtoiClass:
     def myAtoi(self, s):
         """
         :type s: str
         :rtype: int
         """
-        
+        index = 0
+        lstS = list(s)
+        resultString = ""
+        while index < len(s):
+            if lstS[index].isnumeric() is True or lstS[index] == "-":
+                resultString = resultString + lstS[index]
+            index = index + 1
+        return int(resultString)
+
+class MyAtoiClassTest(unittest.TestCase):
+    def test_case1(self):
+        self.assertEqual(MyAtoiClass.myAtoi(self, "words and 987"), 987)
